53eb0763b1ff70bab97653450f6ddb37f35ba32a
gonzalorome7 <93739735+gonzalorome7@users.noreply.github.com>
2021-11-23 13:43:48 -0300

    correctedDetailTedTalkViewController
    corrected guard let in this file.

--- Ted Talk/Ted Talk/ViewController/DetailTedTalkViewController.swift
+++ Ted Talk/Ted Talk/ViewController/DetailTedTalkViewController.swift
@@ -27,21 +27,21 @@ class DetailTedTalkViewController: UIViewController {
         
         super.viewDidLoad()
         
-        guard let displayTedTalk = tedTalk else {
-            return
+        if let displayTedTalk = tedTalk{
+            
+            titleDetail.text = displayTedTalk.title
+            tedTalkWeb.load(URLRequest(url: URL(string: displayTedTalk.url)!))
+            tagString = tagString + (displayTedTalk.tags[0])
+            
+            for tag in 1..<tedTalk!.tags.count {
+                tagString = tagString + ", \(displayTedTalk.tags[tag])"
+            }
+            
+            tagDetail.text = tagString
+            let viewString = displayTedTalk.views as NSNumber
+            ofviewDetail.text = "#of views: \(viewString.stringValue)"
+            nameDetail.text = displayTedTalk.name
+            descriptionDetail.text = displayTedTalk.description
         }
-        titleDetail.text = displayTedTalk.title
-        tedTalkWeb.load(URLRequest(url: URL(string: displayTedTalk.url)!))
-        tagString = tagString + (displayTedTalk.tags[0])
-        
-        for tag in 1..<tedTalk!.tags.count {
-            tagString = tagString + ", \(displayTedTalk.tags[tag])"
-        }
-        
-        tagDetail.text = tagString
-        let viewString = displayTedTalk.views as NSNumber
-        ofviewDetail.text = "#of views: \(viewString.stringValue)"
-        nameDetail.text = displayTedTalk.name
-        descriptionDetail.text = displayTedTalk.description
     }
 }
